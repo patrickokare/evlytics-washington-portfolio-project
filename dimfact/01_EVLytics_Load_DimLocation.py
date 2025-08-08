ae0f9462e5e23bb73e2070f6b5e946eec9e5585d
Patrick Okare <patrickbabawale@gmail.com>
2025-08-08 13:29:34 -0400

    Update 01_EVLytics_Load_DimLocation.py

--- dimfact/01_EVLytics_Load_DimLocation.py
+++ dimfact/01_EVLytics_Load_DimLocation.py
@@ -1,132 +1,118 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC Step I - Deduplication Logic
+# Databricks Notebook
+# Title: DimLocation - SCD Type 2 with Deduplication
+# Author: Patrick Okra
+# Date: 2025-08-08
+# Description: This notebook applies SCD Type 2 logic on location data 
+# using a hashed natural key and deduplication framework.
 
-# COMMAND ----------
-
+# Step I - Deduplication Logic
 dimLocation_df = spark.sql(""" 
     SELECT * 
     FROM (
         SELECT
-        County,
-        City,
-        State,
-        PostalCode,
-        source.`2020CensusTract` AS CensusTract2020,
-        COALESCE(LegislativeDistrict, -1) AS LegislativeDistrict,
-        FileName,
-        current_date() AS EffectiveStartDate,
-        NULL AS EffectiveEndDate,
-        1 AS IsCurrent,
-        source_system AS SourceSystem,
-        NULL AS UpdatedDate,
-        current_timestamp() AS InsertedDate,
+            County,
+            City,
+            State,
+            PostalCode,
+            source.`2020CensusTract` AS CensusTract2020,
+            COALESCE(LegislativeDistrict, -1) AS LegislativeDistrict,
+            FileName,
+            current_date() AS EffectiveStartDate,
+            NULL AS EffectiveEndDate,
+            1 AS IsCurrent,
+            source_system AS SourceSystem,
+            NULL AS UpdatedDate,
+            current_timestamp() AS InsertedDate,
 
-        -- Business grain key
-        sha2(concat_ws('|',
-            COALESCE(County, 'UNKNOWN'),
-            COALESCE(City, 'UNKNOWN'),
-            COALESCE(State, 'UNKNOWN'),
-            COALESCE(PostalCode, '-1'),
-            COALESCE(`2020CensusTract`, 'UNKNOWN')
-        ), 256) AS LocationNaturalKey,
+            -- Business grain key
+            sha2(concat_ws('|',
+                COALESCE(County, 'UNKNOWN'),
+                COALESCE(City, 'UNKNOWN'),
+                COALESCE(State, 'UNKNOWN'),
+                COALESCE(PostalCode, '-1'),
+                COALESCE(`2020CensusTract`, 'UNKNOWN')
+            ), 256) AS LocationNaturalKey,
 
-        -- Change detection
-        sha2(concat_ws('|',
-            COALESCE(County, 'UNKNOWN'),
-            COALESCE(City, 'UNKNOWN'),
-            COALESCE(State, 'UNKNOWN'),
-            COALESCE(PostalCode, '-1'),
-            COALESCE(LegislativeDistrict, '-1'),
-            COALESCE(`2020CensusTract`, 'UNKNOWN')
-        ), 256) AS HASH_ID,
+            -- Change detection
+            sha2(concat_ws('|',
+                COALESCE(County, 'UNKNOWN'),
+                COALESCE(City, 'UNKNOWN'),
+                COALESCE(State, 'UNKNOWN'),
+                COALESCE(PostalCode, '-1'),
+                COALESCE(LegislativeDistrict, '-1'),
+                COALESCE(`2020CensusTract`, 'UNKNOWN')
+            ), 256) AS HASH_ID,
 
-        -- Row number for deduplication
-        ROW_NUMBER() OVER (
-            PARTITION BY sha2(concat_ws('|',
-            COALESCE(County, 'UNKNOWN'),
-            COALESCE(City, 'UNKNOWN'),
-            COALESCE(State, 'UNKNOWN'),
-            COALESCE(PostalCode, '-1'),
-            COALESCE(`2020CensusTract`, 'UNKNOWN')
-            ), 256)
-            ORDER BY current_timestamp()
-        ) AS row_num
+            -- Row number for deduplication
+            ROW_NUMBER() OVER (
+                PARTITION BY sha2(concat_ws('|',
+                    COALESCE(County, 'UNKNOWN'),
+                    COALESCE(City, 'UNKNOWN'),
+                    COALESCE(State, 'UNKNOWN'),
+                    COALESCE(PostalCode, '-1'),
+                    COALESCE(`2020CensusTract`, 'UNKNOWN')
+                ), 256)
+                ORDER BY current_timestamp()
+            ) AS row_num
 
         FROM evlytics_silver.vehiclepopulation AS source 
-        WHERE FileName NOT IN (SELECT DISTINCT FileName FROM evlytics_gold.DimLocation) -- NB: REPLACE with watermark framework table.
+        WHERE FileName NOT IN (
+            SELECT DISTINCT FileName 
+            FROM evlytics_gold.DimLocation
+        )
     ) AS deduped
-    WHERE row_num = 1;
-    """)
+    WHERE row_num = 1
+""")
 
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC Step II -  Register as Temp View
-
-# COMMAND ----------
-
+# Step II - Register as Temp View
 dimLocation_df = dimLocation_df.drop("row_num")
 dimLocation_df.createOrReplaceTempView("vw_DimLocation")
 
-# COMMAND ----------
+# Step III - Merge View into Gold Layer (SCD Type 2)
+spark.sql("""
+    MERGE INTO evlytics_gold.DimLocation AS target
+    USING vw_DimLocation AS source
+    ON target.LocationNaturalKey = source.LocationNaturalKey AND target.IsCurrent = 1
 
-# MAGIC %md
-# MAGIC Step III - Use MERGE from the View into Gold Layer
+    WHEN MATCHED AND (source.HASH_ID <> target.HASH_ID) THEN
+        UPDATE SET
+            target.IsCurrent = 0,
+            target.EffectiveEndDate = current_date() - 1,
+            target.UpdatedDate = current_timestamp()
 
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC -- Step 1: Merge for SCD2 with Hashed Natural Key
-# MAGIC
-# MAGIC MERGE INTO evlytics_gold.DimLocation AS target
-# MAGIC USING vw_DimLocation AS source
-# MAGIC ON target.LocationNaturalKey = source.LocationNaturalKey AND target.IsCurrent = 1
-# MAGIC
-# MAGIC -- Type 2 Change: Only when HASH_ID is different
-# MAGIC WHEN MATCHED AND 
-# MAGIC     (source.HASH_ID <> target.HASH_ID 
-# MAGIC     
-# MAGIC     )THEN
-# MAGIC     UPDATE SET
-# MAGIC         target.IsCurrent = 0,
-# MAGIC         target.EffectiveEndDate = current_date() - 1,
-# MAGIC         target.UpdatedDate = current_timestamp()
-# MAGIC
-# MAGIC -- Insert new records (new or updated)
-# MAGIC WHEN NOT MATCHED THEN
-# MAGIC     INSERT (
-# MAGIC         County,
-# MAGIC         City,
-# MAGIC         State,
-# MAGIC         PostalCode,
-# MAGIC         CensusTract2020,
-# MAGIC         LegislativeDistrict,
-# MAGIC         FileName,
-# MAGIC         EffectiveStartDate,
-# MAGIC         EffectiveEndDate,
-# MAGIC         IsCurrent,
-# MAGIC         SourceSystem,
-# MAGIC         UpdatedDate,
-# MAGIC         InsertedDate,
-# MAGIC         LocationNaturalKey,
-# MAGIC         HASH_ID
-# MAGIC     )
-# MAGIC     VALUES (
-# MAGIC         source.County,
-# MAGIC         source.City,
-# MAGIC         source.State,
-# MAGIC         source.PostalCode,
-# MAGIC         source.CensusTract2020,
-# MAGIC         source.LegislativeDistrict,
-# MAGIC         source.FileName,
-# MAGIC         source.EffectiveStartDate,
-# MAGIC         source.EffectiveEndDate,
-# MAGIC         source.IsCurrent,
-# MAGIC         source.SourceSystem,
-# MAGIC         source.UpdatedDate,
-# MAGIC         source.InsertedDate,
-# MAGIC         source.LocationNaturalKey,
-# MAGIC         source.HASH_ID
-# MAGIC     );
+    WHEN NOT MATCHED THEN
+        INSERT (
+            County,
+            City,
+            State,
+            PostalCode,
+            CensusTract2020,
+            LegislativeDistrict,
+            FileName,
+            EffectiveStartDate,
+            EffectiveEndDate,
+            IsCurrent,
+            SourceSystem,
+            UpdatedDate,
+            InsertedDate,
+            LocationNaturalKey,
+            HASH_ID
+        )
+        VALUES (
+            source.County,
+            source.City,
+            source.State,
+            source.PostalCode,
+            source.CensusTract2020,
+            source.LegislativeDistrict,
+            source.FileName,
+            source.EffectiveStartDate,
+            source.EffectiveEndDate,
+            source.IsCurrent,
+            source.SourceSystem,
+            source.UpdatedDate,
+            source.InsertedDate,
+            source.LocationNaturalKey,
+            source.HASH_ID
+        )
+""")
